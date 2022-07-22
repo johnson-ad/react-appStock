5c0a7243083aef477d5d15e7c9fd84160a7ee64b
='Johnson-ad <=>
2022-07-22 01:37:51 +0000

    Relation entre produit entree sortie categorie

--- server/index.js
+++ server/index.js
@@ -33,18 +33,71 @@ async function createSortie(quatite, prix, date) {
     return await sortie.save();
 }
 
+async function createProduct(categorie, libelle, stock, entree, sortie) {
+    const product = new ProductModel({ categorie, libelle, stock, entree, sortie });
+    return await product.save();
+};
+
+async function createCategorie(nom) {
+    const categorie = new CategorieModel({ nom });
+    return await categorie.save();
+}
+
+
+
 // section show functon
 async function showUsers() {
     return await UserModel.find().populate('role');
 }
 
+async function showProducts() {
+    return await ProductModel.find();
+}
+
+async function showCategories() {
+    return await CategorieModel.find();
+}
+
+async function showEntrees() {
+    return await EntreeModel.find();
+}
+
+async function showSorties() {
+    return await SortieModel.find();
+}
+
 //section declare function
-createRole('admin').then((role) => {
-    console.log('role created...');
-    const user = new UserModel({ role: role, nom: 'admin', prenom: 'admin', email: 'admin@gmail.com', password: 'admin' });
-    return user.save().then(() => {
-        console.log('first user created...');
-        showUsers();
+// createRole('admin').then((role) => {
+//     console.log('role created...');
+//     const user = new UserModel({ role: role, nom: 'admin', prenom: 'admin', email: 'admin@gmail.com', password: 'admin' });
+//     return user.save().then(() => {
+//         console.log('first user created...');
+//         showUsers();
+//     })
+// });
+
+createCategorie('Pc portable').then((categorie) => {
+    console.log('first categorie created...');
+    // showCategories();
+    const entree = new EntreeModel({ quatite: 60, prix: 1800, date: new Date(Date.now()) });
+    return entree.save().then(() => {
+        console.log('first entree created...');
+        // showEntrees();
+        const sortie = new SortieModel({ quatite: 3, prix: 1500, date: new Date(Date.now()) });
+        return sortie.save().then((entree) => {
+            console.log('first sortie created...');
+            // showSorties();
+            const product = new ProductModel({ categorie: categorie, libelle: 'MacBook Air', stock: 100, entree: entree, sortie: sortie });
+            return product.save().then(() => {
+                console.log('first product created...');
+                showProducts();
+            })
+        })
     })
 });
-
+    // const produit = new ProductModel({ categorie: categorie, libelle: 'MacBook Pro', stock: 120 });
+    // return produit.save().then(() => {
+    //     console.log('first product created...');
+    //     showProducts();
+    // }).then(() => {
+    //     return createEntree(10, 1000, new Date(Date.now())).then((entree) => {
--- server/model/model.js
+++ server/model/model.js
@@ -2,17 +2,9 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 
 const CategorieSchema = new mongoose.Schema({
-    nom: {
-        type: String,
-        required: true,
-        unique: true
-    }
-    ,
-    produit: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'produits'
-    }
+    nom: String
 });
+
 const CategorieModel = mongoose.model('categories', CategorieSchema);
 
 const EntreeSchema = new mongoose.Schema({
@@ -32,33 +24,6 @@ const EntreeSchema = new mongoose.Schema({
 
 const EntreeModel = mongoose.model('entree', EntreeSchema);
 
-const ProductSchema = new mongoose.Schema({
-    categorie: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'categories'
-    },
-    libelle: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    stock: {
-        type: Number,
-        required: true
-    }
-});
-
-const ProductModel = mongoose.model('produits', ProductSchema);
-
-const RoleSchema = new mongoose.Schema({
-    nom: {
-        type: String,
-        required: true
-    }
-});
-
-const RoleModel = mongoose.model('role', RoleSchema);
-
 const SortieSchema = new mongoose.Schema({
     quatite: {
         type: Number,
@@ -76,11 +41,34 @@ const SortieSchema = new mongoose.Schema({
 
 const SortieModel = mongoose.model('sortie', SortieSchema);
 
-const UserSchema = new mongoose.Schema({
-    role: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'role'
+const ProductSchema = new mongoose.Schema({
+    categorie: CategorieSchema,
+    libelle: {
+        type: String,
+        required: true,
+        unique: true
     },
+    stock: {
+        type: Number,
+        required: true
+    },
+    entree: EntreeSchema,
+    sortie: SortieSchema,
+});
+
+const ProductModel = mongoose.model('produits', ProductSchema);
+
+const RoleSchema = new mongoose.Schema({
+    nom: {
+        type: String,
+        required: true
+    }
+});
+
+const RoleModel = mongoose.model('role', RoleSchema);
+
+const UserSchema = new mongoose.Schema({
+    role: RoleSchema,
     nom: {
         type: String,
         required: true
@@ -91,15 +79,14 @@ const UserSchema = new mongoose.Schema({
     },
     email: {
         type: String,
-        // required: true,
-        // unique: true,
-        // validate verifies if the email is valid
-        // validate: (value) => {
-        //     if (!validator.isEmail(value)) {
-        //         throw new Error('Invalid Email');
-        //     }
-        // }
-
+        required: true,
+        unique: true,
+        //validate verifies if the email is valid
+        validate: (value) => {
+            if (!validator.isEmail(value)) {
+                throw new Error('Invalid Email');
+            }
+        }
     },
     password: {
         type: String,
